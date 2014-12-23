24cccb2ab92e5f8bdc1b5d2dedf1850d5f9cb528
Erin Jo Richey <richey.erin@gmail.com>
2014-12-23 13:19:00 -0800

    Formatting changes for account page

--- templates/default/account/soundcloud.tpl.php
+++ templates/default/account/soundcloud.tpl.php
@@ -1,7 +1,7 @@
 <div class="row">
 
     <div class="span10 offset1">
-	            <?=$this->draw('account/menu')?>
+        <?=$this->draw('account/menu')?>
         <h1>Soundcloud</h1>
 
     </div>
@@ -14,109 +14,49 @@
                 if (empty(\Idno\Core\site()->session()->currentUser()->soundcloud)) {
             ?>
                     <div class="control-group">
-                        <div class="controls">
-                            <p>
-                                If you have a Soundcloud account, you may connect it here. Public content that you
-                                post to this site will be automatically cross-posted to your Soundcloud wall.
+                        <div class="controls-config">
+	                        <div class="row">
+						   		<div class="span6">
+                                <p>
+                                Easily share audio to Soundcloud.</p>  
+                                
+                                <p>
+                                With Soundcloud connected, you can cross-post audio that you publish publicly on your site. 
                             </p>
-                            <?php
-
-                                if (empty($vars['login_url'])) {
-
-                                    if (\Idno\Core\site()->session()->isAdmin()) {
-
-                                        ?>
-                                        <p>
-                                            You need to set up an API integration with SoundCloud before you connect
-                                            your account. <a href="<?=\Idno\Core\site()->config()->getDisplayURL()?>admin/soundcloud/">Click here to get started.</a>
-                                        </p>
-                                        <?php
-
-                                    } else {
-
-                                        ?>
-                                        <p>
-                                            SoundCloud isn't available right now. Please try again later.
-                                        </p>
-                                        <?php
-
-                                    }
-
-                                } else {
-
-                                    ?>
-                                    <p>
-                                        <a href="<?= $vars['login_url'] ?>" class="btn btn-large btn-success">Click here
-                                            to connect SoundCloud to your account</a>
-                                    </p>
-                                <?php
-
-                                }
-
-                            ?>
+						   		</div>
+	                        </div>
+	                        <div class="social span4">
+                            <p>
+                                <a href="<?=$vars['login_url']?>" class="connect scld">Connect Soundcloud</a>
+                            </p>
+	                        </div>
                         </div>
                     </div>
-                <?php
-
-                } else if (!\Idno\Core\site()->config()->multipleSyndicationAccounts()) {
-
-                    ?>
-                    <div class="control-group">
-                        <div class="controls">
-                            <p>
-                                Your account is currently connected to SoundCloud. Public content that you post
-                                here
-                                will be shared with your SoundCloud account.
-                            </p>
-
-                            <p>
-                                <input type="hidden" name="remove" value="1"/>
-                                <button type="submit" class="btn btn-primary">Click here to remove SoundCloud from
-                                    your account.
-                                </button>
-                            </p>
-                        </div>
-                    </div>
-
                 <?php
 
                 } else {
 
                     ?>
                     <div class="control-group">
-                        <div class="controls">
-                            <p class="explanation">
-                                You have connected the following accounts to SoundCloud:
-                            </p>
-                            <?php
-
-                                if ($accounts = \Idno\Core\site()->syndication()->getServiceAccounts('soundcloud')) {
-
-                                    foreach ($accounts as $account) {
-
-                                        ?>
-                                        <p>
-                                            <input type="hidden" name="remove" value="<?= $account['username'] ?>"/>
-                                            <button type="submit"
-                                                    class="btn btn-primary"><?= $account['username'] ?></button>
-                                        </p>
-                                    <?php
-
-                                    }
-
-                                }
-
-                            ?>
+                        <div class="controls-config">
+	                     <div class="row">
+						    <div class="span6">
                             <p>
-                                <a href="<?= $vars['login_url'] ?>" class="">Click here
-                                    to connect another SoundCloud account</a>
+                                Your account is currently connected to Soundcloud. Public content that you post here
+                                will be shared with your Soundcloud account.
+                            </p>
+						    </div>
+	                     </div>
+                            <p>
+                                <input type="hidden" name="remove" value="1" />
+                                <button type="submit" class="btn btn-primary">Disconnect Soundcloud</button>
                             </p>
                         </div>
                     </div>
+
                 <?php
 
                 }
-
             ?>
             <?= \Idno\Core\site()->actions()->signForm('/account/soundcloud/')?>
         </form>
