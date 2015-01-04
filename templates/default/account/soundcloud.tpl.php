d3b6892efcac79d75a7f219083edd1aa5b36f818
Ben Werdmuller <ben@benwerd.com>
2015-01-03 19:24:54 -0800

    Improved account connection screen

--- templates/default/account/soundcloud.tpl.php
+++ templates/default/account/soundcloud.tpl.php
@@ -1,7 +1,7 @@
 <div class="row">
 
     <div class="span10 offset1">
-        <?=$this->draw('account/menu')?>
+        <?= $this->draw('account/menu') ?>
         <h1>Soundcloud</h1>
 
     </div>
@@ -9,58 +9,87 @@
 </div>
 <div class="row">
     <div class="span10 offset1">
-        <form action="/account/soundcloud/" class="form-horizontal" method="post">
+        <form action="<?= \Idno\Core\site()->config()->getDisplayURL() ?>account/soundcloud/" class="form-horizontal"
+              method="post">
             <?php
                 if (empty(\Idno\Core\site()->session()->currentUser()->soundcloud)) {
-            ?>
+                    ?>
                     <div class="control-group">
                         <div class="controls-config">
-	                        <div class="row">
-						   		<div class="span6">
+                            <div class="row">
+                                <div class="span6">
+                                    <p>
+                                        Easily share audio to Soundcloud.</p>
+
+                                    <p>
+                                        With Soundcloud connected, you can cross-post audio that you publish publicly on
+                                        your site.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="social span4">
                                 <p>
-                                Easily share audio to Soundcloud.</p>  
-                                
-                                <p>
-                                With Soundcloud connected, you can cross-post audio that you publish publicly on your site. 
-                            </p>
-						   		</div>
-	                        </div>
-	                        <div class="social span4">
-                            <p>
-                                <a href="<?=$vars['login_url']?>" class="connect scld">Connect Soundcloud</a>
-                            </p>
-	                        </div>
+                                    <a href="<?= $vars['login_url'] ?>" class="connect scld">Connect Soundcloud</a>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 <?php
 
-                } else {
+                } else if (!\Idno\Core\site()->config()->multipleSyndicationAccounts()) {
 
                     ?>
                     <div class="control-group">
                         <div class="controls-config">
-	                     <div class="row">
-						    <div class="span6">
-                            <p>
-                                Your account is currently connected to Soundcloud. Public content that you post here
-                                will be shared with your Soundcloud account.
-                            </p>
-						    </div>
-	                     </div>
-	                     <div class="social">
-                            <p>
-                                <input type="hidden" name="remove" value="1" />
-                                <button type="submit" class="connect scld connected">Disconnect Soundcloud</button>
-                            </p>
-	                     </div>
+                            <div class="row">
+                                <div class="span6">
+                                    <p>
+                                        Your account is currently connected to Soundcloud. Public content that you post
+                                        here
+                                        will be shared with your Soundcloud account.
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="social">
+                                <p>
+                                    <input type="hidden" name="remove" value="1"/>
+                                    <button type="submit" class="connect scld connected">Disconnect Soundcloud</button>
+                                </p>
+                            </div>
                         </div>
                     </div>
+
+                <?php
+
+                } else {
+
+                    if ($accounts = \Idno\Core\site()->syndication()->getServiceAccounts('soundcloud')) {
+
+                        foreach ($accounts as $account) {
+
+                            ?>
+                            <p>
+                                <input type="hidden" name="remove" value="<?= $account['username'] ?>"/>
+                                <button type="submit"
+                                        class="connect scld connected"><?= $account['username'] ?></button>
+                            </p>
+                        <?php
+
+                        }
+
+                    }
+
+                    ?>
+                    <p>
+                        <a href="<?= $vars['login_url'] ?>" class="">Click here
+                            to connect another SoundCloud account</a>
+                    </p>
 
                 <?php
 
                 }
             ?>
-            <?= \Idno\Core\site()->actions()->signForm('/account/soundcloud/')?>
+            <?= \Idno\Core\site()->actions()->signForm('/account/soundcloud/') ?>
         </form>
     </div>
 </div>
